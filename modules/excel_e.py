059a1b76442440fa29e1850d208e4ffa0d8185fd
Nafil <144507967+Nafilcio@users.noreply.github.com>
2026-04-02 13:46:09 +0700

    Update excel_e.py

--- modules/excel_e.py
+++ modules/excel_e.py
@@ -255,7 +255,7 @@ def save_to_excel_e(df_sc, df_benefit, filename: str):
         def autofit(sheet, df):
             for idx, col in enumerate(df.columns):
                 series = df[col].astype(str).replace("nan", "")
-                max_len = max(series.map(len).max(), len(col))
+                max_len = max(series.astype(str).str.len().max(), len(col))
                 sheet.set_column(idx, idx, max_len + 2)
 
         autofit(sc, df_sc)
