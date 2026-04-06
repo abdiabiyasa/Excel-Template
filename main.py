fad4e1d0e9bef83b4c139e6451393a28449fa559
Nafil <144507967+Nafilcio@users.noreply.github.com>
2026-04-06 15:00:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,8 @@ elif st.session_state.option == "SC + Benefit":
         filename = st.text_input("Enter Excel filename:", "SC & Benefit - - YTD", key="fname_c")
 
         if filename:
-            df_sc = df_sc.replace([np.nan, np.inf, -np.inf], "")
+            df_sc = df_sc.copy()
+            df_sc = df_sc.replace([np.inf, -np.inf], np.nan).fillna "")
             df_benefit = df_benefit.replace([np.nan, np.inf, -np.inf], "")
 
             excel_bytes, fname = save_to_excel_c(df_sc, df_benefit, filename + ".xlsx")
